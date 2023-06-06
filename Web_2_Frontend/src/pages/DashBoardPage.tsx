dd02830c805a45307f2402e2ceb76c80f9736429
Kimara1904 <mikipaok@gmail.com>
2023-06-06 16:17:44 +0200

    Fixing minor issues

--- Web_2_Frontend/src/pages/DashBoardPage.tsx
+++ Web_2_Frontend/src/pages/DashBoardPage.tsx
@@ -1,23 +1,11 @@
-import { ReactNode } from 'react'
-
-import { Typography } from '@mui/material'
-
-import { isAdmin, isCustomer, isSellerVerified } from '../helpers/TokenHelpers'
 import DashboardContent from '../components/Dashboard/DashboardContent'
 
 const DashboardPage = () => {
-  let content: ReactNode = null
-
-  if (!isAdmin() && !isSellerVerified() && !isCustomer()) {
-    content = (
-      <Typography variant='h2' color='error' align='center' style={{ marginBottom: '16px' }}>
-        You&apos;re not verified
-      </Typography>
-    )
-  } else {
-    content = <DashboardContent />
-  }
-  return <>{content}</>
+  return (
+    <>
+      <DashboardContent />
+    </>
+  )
 }
 
 export default DashboardPage
